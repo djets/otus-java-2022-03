18e5ab37e5d9974f3c2164d1ea73b4682008ea96
djets <jag0s3@gmail.com>
2022-05-18 20:04:38 +0300

    modified:   L12/Bank/src/Bank.java

--- L12/Bank/src/Bank.java
+++ L12/Bank/src/Bank.java
@@ -5,9 +5,14 @@ public class Bank {
     final static TreeMap <Integer, Client> accountsClients = new TreeMap<>();
 
     public static void main(String[] args) {
-        createNewClientsAndAccounts(50, 10, 1000000000);
+        int numberOfClients = 50000;
+        int numberOfCostumeAccounts = 10;
+        int lengthAccountNumber = 1000000000;
+
+
+        createNewClientsAndAccounts(numberOfClients, numberOfCostumeAccounts, lengthAccountNumber);
         try {
-            listAccounts(getListAccounts(clientsName.get(inNameClient())));
+            listAccounts(getListAccounts(clientsName.get(inNameClient(numberOfClients))));
         } catch (Exception e) {
             System.out.print("Нет такого клиента.");
         }
@@ -19,14 +24,19 @@ public class Bank {
         }
     }
     static void createNewClientsAndAccounts(int c, int a, int r){
+        HashSet<Integer> UsedAccountNumber = new HashSet<>();
         for (int i = 0; i <= c; i++){
-        newClient("client" + i);
-        int numberOfAccounts = (int) (Math.random() * a);
-        for (int n = 0; n <= numberOfAccounts; n++){
-            int numAccount = (int) (Math.random()*r);
-            clientsName.get("client" + i).addAccount(numAccount);
-            accountsClients.put(numAccount, clientsName.get("client" + i));
-        }
+            newClient("client" + i);
+            int numberOfAccounts = (int) (Math.random() * a);
+                for (int n = 0; n <= numberOfAccounts; n++){
+                    int numAccount = (int) (Math.random()*r);
+                    while (UsedAccountNumber.equals(numAccount));{
+                        numAccount = (int) (Math.random()*r);
+                    }
+                    UsedAccountNumber.add(numAccount);
+                    clientsName.get("client" + i).addAccount(numAccount);
+                    accountsClients.put(numAccount, clientsName.get("client" + i));
+                }
         }
     }
     static void newClient(String name){
@@ -40,9 +50,9 @@ public class Bank {
     static LinkedList<Account> getListAccounts(Client client){
         return client.getAccounts();
     }
-    static String inNameClient(){
+    static String inNameClient(int c){
         Scanner inStr =  new Scanner(System.in);
-        System.out.print("Введите имя клиента: ");
+        System.out.print("Введите имя клиента вида client*, где * число от 0 до " + c + ":\n");
         if (inStr.hasNextLine()){
             String str = inStr.nextLine();
             if(clientsName.containsKey(str)){
