11982b09198927f5735fcba5a59b0c018c11ddeb
djets <jag0s3@gmail.com>
2022-06-07 16:15:07 +0300

    L17 fix

--- L17/homework.sql
+++ L17/homework.sql
@@ -8,100 +8,113 @@ CREATE DATABASE otus_l17
     ENCODING = 'UTF8'
     
 -- Создаем таблицы вопросов и ответов
--- Questions --1--*--> Answers
+-- Questions <--*--*--> Answers
 
 ------------------------------------
--- Questions
-------------------------------------
-
-CREATE TABLE questions
-(
-    question_id     SERIAL          PRIMARY KEY,
-    question        VARCHAR(200)    NOT NULL UNIQUE
-);
-
-DROP TABLE questions
-
--- Заполняем таблицу вопросов
-
-INSERT INTO questions(question)
-VALUES      ('Кто был звездой фильма 1973 года «Занджир»?'),
-            ('На национальной эмблеме Индии изображены три льва. А что написано под ними?'),
-            ('Что он традиционно держит в правой руке в изображениях бога Рамы?'),
-            ('Песня Даршан До Гханшьям - произведение великого индийского поэта; как зовут этого поэта?'),
-            ('На 100-долларовой купюре есть портрет известного американского государственного деятеля?'),
-            ('Кто изобретатель револьвера?'),
-            ('В каком городе находится Кембриджский цирк?'),
-            ('Какой игрок в крикет забил больше всего «сотен» в официальных матчах?'),
-            ('В шедевре Александра Дюма «Три мушкетера» двух мушкетеров зовут Атос и Портос . Как зовут третьего мушкетера?');
-
-SELECT * FROM questions
-
-DELETE FROM questions
-    WHERE question = 'Кто был звездой фильма 1973 года «Занджир»?';
-
-
-------------------------------------
--- Answers
+-- Questions, Answers, Relationship
 ------------------------------------
 
 CREATE TABLE answers
 (
     answers_id      SERIAL          PRIMARY KEY,
-    answer          VARCHAR(100)    NOT NULL,
-    question_id     SERIAL          REFERENCES questions (question_id),
-    correct         BOOLEAN         DEFAULT FALSE  
+    answer          VARCHAR(100)    NOT NULL UNIQUE      
 );
-
 SELECT * FROM answers
 
-INSERT INTO answers(answer, question_id, correct)
-VALUES      ('Амитабх Баччан', 3, true),
-            ('Анил Капур', 3, false),
-            ('Шахрукх Кхан', 3, false),
-            ('Мадхур Миттал', 3, false),
-            ('Только правда побеждает', 4, true),
-            ('Только торжество лжи', 4, false),
-            ('Только триумфы моды', 4, false),
-            ('Только деньги торжествуют', 4, false),
-            ('Цветок', 5, false),
-            ('Меч', 5, false),
-            ('Ребенок', 5, false),
-            ('Лук и стрела', 5, true),
-            ('Сурдас', 6, true),
-            ('Тулсидас', 6, false),
-            ('Мирабай', 6, false),
-            ('Кабир', 6, false),
-            ('Джордж Вашингтон', 7, false),
-            ('Франклин Рузвельт', 7, false),
-            ('Бенджамин Франклин', 7, true),
-            ('Абрахам Линкольн', 7, false),
-            ('Сэмюэл Кольт', 8, true),
-            ('Брюс Браунинг', 8, false),
-            ('Дэн Вессон', 8, false),
-            ('Револьвер Джеймса', 8, false),
-            ('Оксфорд', 9, false),
-            ('Лидс', 9, false),
-            ('Кембридж', 9, false),
-            ('Лондон', 9, true),
-            ('Сачин Тендулкар', 10, false),
-            ('Рики Понтинг', 10, false),
-            ('Майкл Слейтер', 10, false),
-            ('Джек Хоббс', 10, true),
-            ('Арамис', 11, false),
-            ('Кардинал Ришелье', 11, false),
-            ('Д Артаньян', 11, false),
-            ('Планше', 11, false);
+CREATE TABLE questions
+(
+    question_id             SERIAL          PRIMARY KEY,
+    question                VARCHAR(200)    NOT NULL UNIQUE,
+    correct_answer_id       SERIAL          REFERENCES answers (answers_id)
+);
+SELECT * FROM questions
+
+CREATE TABLE question_to_answer
+(
+question_id     SERIAL      REFERENCES questions (question_id),
+answer_id       SERIAL      REFERENCES answers (answers_id)   
+);
+SELECT * FROM question_to_answer
+
+-- Filling
+
+INSERT INTO answers(answer)
+VALUES      ('Амитабх Баччан'),
+            ('Анил Капур'),
+            ('Шахрукх Кхан'),
+            ('Мадхур Миттал'),
+            ('Только правда побеждает'),
+            ('Только торжество лжи'),
+            ('Только триумфы моды'),
+            ('Только деньги торжествуют'),
+            ('Цветок'),
+            ('Меч'),
+            ('Ребенок'),
+            ('Лук и стрела'),
+            ('Сурдас'),
+            ('Тулсидас'),
+            ('Мирабай'),
+            ('Кабир'),
+            ('Джордж Вашингтон'),
+            ('Франклин Рузвельт'),
+            ('Бенджамин Франклин'),
+            ('Абрахам Линкольн'),
+            ('Сэмюэл Кольт'),
+            ('Брюс Браунинг'),
+            ('Дэн Вессон'),
+            ('Револьвер Джеймса'),
+            ('Оксфорд'),
+            ('Лидс'),
+            ('Кембридж'),
+            ('Лондон'),
+            ('Сачин Тендулкар'),
+            ('Рики Понтинг'),
+            ('Майкл Слейтер'),
+            ('Джек Хоббс'),
+            ('Арамис'),
+            ('Кардинал Ришелье'),
+            ('Д Артаньян'),
+            ('Планше');
 
 SELECT * FROM answers  
 
--- Обновляем
+INSERT INTO questions(question, correct_answer_id)
+VALUES      ('Кто был звездой фильма 1973 года «Занджир»?', 1),
+            ('На национальной эмблеме Индии изображены три льва. А что написано под ними?', 5),
+            ('Что он традиционно держит в правой руке в изображениях бога Рамы?', 12),
+            ('Песня Даршан До Гханшьям - произведение великого индийского поэта; как зовут этого поэта?', 13),
+            ('На 100-долларовой купюре есть портрет известного американского государственного деятеля?', 19),
+            ('Кто изобретатель револьвера?', 21),
+            ('В каком городе находится Кембриджский цирк?', 28),
+            ('Какой игрок в крикет забил больше всего «сотен» в официальных матчах?', 32),
+            ('В шедевре Александра Дюма «Три мушкетера» двух мушкетеров зовут Атос и Портос . Как зовут третьего мушкетера?', 33);
+
+-- DELETE FROM questions
+--    WHERE question = 'Кто был звездой фильма 1973 года «Занджир»?';
+
+SELECT * FROM questions 
+
+DO
+$f$
+BEGIN
+    FOR q IN 1..9 LOOP        
+        FOR a IN REVERSE 3..0 LOOP
+            INSERT INTO question_to_answer (question_id, answer_id)
+            VALUES (q, q * 4 - a);
+        END LOOP;    
+    END LOOP;
+END;
+$f$;
+
+SELECT * FROM question_to_answer
+    
+-- Update
 
 UPDATE answers
 SET answer = 'Д Артаньян'
 WHERE answer = 'Д''Артаньян';
 
--- Ищем совпадения по части
+-- Search
 
 SELECT * FROM answers
 WHERE answer LIKE '%ян';
@@ -112,15 +125,9 @@ SELECT q.*, a.*
 FROM questions AS q
 CROSS JOIN answers a
 
--- Вывод ответов/вопрос 
-
-SELECT answers.*, questions.*
+-- Вывод ответов/правильный ответ 
+SELECT questions.question, answers.answer
 FROM answers
-JOIN questions ON questions.question_id = answers.question_id;
-            
--- Вывод вопрос/правильный ответ
+JOIN questions ON questions.correct_answer_id = answers.answers_id;
 
-SELECT q.question, a.answer AS question
-FROM answers AS a
-JOIN questions q ON q.question_id = a.question_id
-WHERE correct = true;
+
